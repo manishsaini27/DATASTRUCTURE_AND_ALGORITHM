817940e2458a6c1e10466368097f83e048df8309
Manish Saini <saini36785@gmail.com>
2026-09-13 17:03:38 +0530

    new question added

---
+++ 2D_Array_2/Spiralprinting.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the number of rows : ";
+    cin>> m;
+
+    int n;
+    cout<<"Enter the number of the columns : ";
+    cin>>n;
+
+    vector<vector<int>>matrix(m,vector<int>(n));
+
+    cout<<"Enter the element of the matrics : ";
+    cout<<endl;
+
+    for(int i =0; i<m; i++) {
+        for(int j =0; j<n; j++) {
+            cin>>matrix[i][j];
+        }
+    }
+    //spiral
+    int minr =0;
+    int minc=0;
+    int maxr = m-1;
+    int maxc = n-1;
+
+    while(minr<=maxr && minc<=maxc){
+        //rigth
+        for(int j=minc; j<=maxc ;j++){
+            cout<<matrix[minr][j] <<" ";
+        }
+        minr++;
+
+        if(minr>maxr || minc>maxc) break;
+
+        //down
+        for(int i= minr; i<=maxr ;i++) {
+            cout<<matrix[i][maxc]<<" ";
+        }
+        maxc--;
+        
+        if(minr>maxr || minc>maxc) break;
+
+        //left
+        for(int j=maxc; j>=minc;j--) {
+            cout<<matrix[maxr][j]<<" ";
+        }
+        maxr--;
+        if(minr>maxr || minc>maxc) break;
+
+        //up
+        for(int i=maxr ;i>=minr ;i--){
+            cout<<matrix[i][minc]<<" ";
+        }
+        minc++;
+        if(minr>maxr || minc>maxc) break;
+    }
+
+}
+
+
+
+// #include<iostream>
+// #include<vector>
+
+// using namespace std;
+// int main(){
+//     int m;
+//     cout<<"Enter the number of rows : ";
+//     cin>> m;
+
+//     int n;
+//     cout<<"Enter the number of the columns : ";
+//     cin>>n;
+
+//     vector<vector<int>>matrix(m,vector<int>(n));
+
+//     cout<<"Enter the element of the matrics : ";
+//     cout<<endl;
+
+//     for(int i =0; i<m; i++) {
+//         for(int j =0; j<n; j++) {
+//             cin>>matrix[i][j];
+//         }
+//     }
+//     //spiral
+//     int minr =0;
+//     int minc=0;
+//     int maxr = m-1;
+//     int maxc = n-1;
+//     int tne = m*n;
+//     int count = 0;
+
+//     while(minr<=maxr && minc<=maxc){
+//         //rigth
+//         for(int j=minc; j<=maxc && count<tne ;j++){
+//             cout<<matrix[minr][j] <<" ";
+//             count++;
+//         }
+//         minr++;
+
+//         //down
+//         for(int i= minr; i<=maxr && count<tne ;i++) {
+//             cout<<matrix[i][maxc]<<" ";
+//             count++;
+//         }
+//         maxc--;
+//         //left
+//         for(int j=maxc; j>=minc && count<tne;j--) {
+//             cout<<matrix[maxr][j]<<" ";
+//             count++;
+//         }
+//         maxr--;
+    
+//         //up
+//         for(int i=maxr ;i>=minr&& count<tne ;i--){
+//             cout<<matrix[i][minc]<<" ";
+//             count++;
+//         }
+//         minc++;
+        
+//     }
+
+// }
--- 2D_Array_2/WavePrint3.cpp
+++ 2D_Array_2/WavePrint3.cpp
@@ -22,5 +22,20 @@ int main(){
         }
     }
 
-    
+    for(int j=0;j<n;j++) {
+        if(j%2 == 0) {
+            for(int i=0;i<m;i++) {
+                cout<<matrix[i][j]<<" ";
+            }
+        }
+        else{
+            for(int i=m-1; i>=0;i--) {
+                cout<<matrix[i][j]<<" ";
+            }
+        }
+    }
+
+     
+
+
 }    
